d4b0bce7f075c479d6d5c1b7236ab50ef1edf957
Johannes Buchner <jbuchner@mpe.mpg.de>
2012-08-07 14:12:15 +0200

    plotting best fit as well; some syntax cleanup

--- pyblocxs/mn.py
+++ pyblocxs/mn.py
@@ -22,29 +22,27 @@ def create_jeffreys_prior_for(par):
     return lambda x: 10**(x * spread + low)
 
 
-def create_prior_function(id=None, otherids=(), priors = []):
-    fit = ui._session._get_fit(id, otherids)[1]
-
-    if not isinstance(fit.stat, (Cash, CStat)):
-        raise RuntimeError("Fit statistic must be cash or cstat, not %s" %
-                           fit.stat.name)
-
-    thawedparmins  = fit.model.thawedparmins
-    thawedparmaxes = fit.model.thawedparmaxes
+def create_prior_function(id=None, otherids=(), priors = [], parameters = None):
     
     functions = []
     if priors == []:
+        assert parameters is not None, "you need to pass the parameters if you want automatic priors"
+        thawedparmins  = [p.min for p in parameters]
+        thawedparmaxes = [p.max for p in parameters]
         for low, high, i in zip(thawedparmins, thawedparmaxes, range(ndim)):
-            functions.append(lambda x: x * (high-low) + low)
+            functions.append(lambda x: x * (high - low) + low)
     else:
         functions = priors
     
     def prior_function(cube, ndim, nparams):
-	for i in range(ndim):
-	    cube[i] = functions[i](cube[i])
+        for i in range(ndim):
+            cube[i] = functions[i](cube[i])
+
     return prior_function
 
-def nested_run(id=None, otherids=(), prior = None, sampling_efficiency = 0.8, 
+plot_best = False
+
+def nested_run(id=None, otherids=(), prior = None, parameters = None, sampling_efficiency = 0.8, 
     n_live_points = 1000, outputfiles_basename = 'chains/', **kwargs):
     fit = ui._session._get_fit(id, otherids)[1]
 
@@ -52,26 +50,45 @@ def nested_run(id=None, otherids=(), prior = None, sampling_efficiency = 0.8,
         raise RuntimeError("Fit statistic must be cash or cstat, not %s" %
                            fit.stat.name)
     
+    global bestl
+    bestl = -0.5*fit.calc_stat()
+    if parameters is None:
+	parameters = fit.model.thawedpars
     def log_likelihood(cube, ndim, nparams):
-        params = [cube[i] for i in range(ndim)]
-        fit.model.thawedpars = params
-        l = -0.5*fit.calc_stat()
-        #print "%.2e " * ndim % tuple([float(p) for p in fit.model.thawedpars]), "%.0f" % l
-        return l
+        try:
+          for i, p in enumerate(parameters):
+            p.val = cube[i]
+          l = -0.5*fit.calc_stat()
+          global bestl
+          global plot_best
+          if plot_best and l > bestl:
+            for i, p in enumerate(parameters):
+              if p.name == 'norm' or p.name == 'ampl':
+                print "%s=%e" % (p.fullname, p.val),
+              else:
+                print "%s=%f" % (p.fullname, p.val),
+            print
+            bestl = l
+            ui.plot_fit_resid()
+          #print "%.2e " * ndim % tuple([float(p) for p in fit.model.thawedpars]), "%.0f" % l
+          return l
+        except Exception as e:
+          print 'Exception in log_likelihood function: ', e
+          import sys
+          sys.exit(-127)
+          return -1e300
+          
     
     if prior is None:
-        prior = create_prior_function(id, otherids)
-    
-    n_params = len(fit.model.thawedpars)
+        prior = create_prior_function(id=id, otherids=otherids, parameters = parameters)
+    n_params = len(parameters)
     pymultinest.run(log_likelihood, prior, n_params, 
         sampling_efficiency = sampling_efficiency, n_live_points = n_live_points, 
         outputfiles_basename = outputfiles_basename, **kwargs)
     
     import json
     m = ui._session._get_model(id)
-    paramnames = map(lambda x: x.name, filter(lambda x: not x.frozen, m.pars))
+    paramnames = map(lambda x: x.fullname, parameters)
     json.dump(paramnames, file('%sparams.json' % outputfiles_basename, 'w'), indent=2)
-
-
 
 
