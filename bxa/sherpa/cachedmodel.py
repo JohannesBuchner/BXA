7e1f873bd94ec4cfb8599ad4257f88856a26d0ce
Johannes Buchner <johannes.buchner.acad@gmx.com>
2016-11-25 14:12:41 +0100

    modified caching to not look at parameters

--- bxa/sherpa/cachedmodel.py
+++ bxa/sherpa/cachedmodel.py
@@ -4,7 +4,7 @@
 """
 BXA (Bayesian X-ray Analysis) for Sherpa
 
-Copyright: Johannes Buchner (C) 2013-2015
+Copyright: Johannes Buchner (C) 2013-2016
 """
 
 import os
@@ -16,7 +16,7 @@ if 'MAKESPHINXDOC' not in os.environ:
 import numpy
 
 from sherpa.models import ArithmeticModel, CompositeModel
-class CachedModel(CompositeModel, ArithmeticModel):
+class VariableCachedModel(CompositeModel, ArithmeticModel):
 	def __init__(self, othermodel):
 		self.othermodel = othermodel
 		self.cache = None
@@ -29,6 +29,33 @@ class CachedModel(CompositeModel, ArithmeticModel):
 			print '   recomputing cached model ... '
 			self.cache = self.othermodel.calc(p, left, right, *args, **kwargs)
 			self.lastp = p
+		return self.cache
+
+	def startup(self):
+		self.othermodel.startup()
+		CompositeModel.startup(self)
+	
+	def teardown(self):
+		self.othermodel.teardown()
+		CompositeModel.teardown(self)
+
+	def guess(self, dep, *args, **kwargs):
+		self.othermodel.guess(dep, *args, **kwargs)
+		CompositeModel.guess(self, dep, *args, **kwargs)
+
+class CachedModel(CompositeModel, ArithmeticModel):
+	def __init__(self, othermodel):
+		self.othermodel = othermodel
+		self.cache = None
+		self.lastp = None
+		print 'calling CompositeModel...'
+		CompositeModel.__init__(self, name='cached(%s)' % othermodel.name, parts=(othermodel,))
+	
+	def calc(self, p, left, right, *args, **kwargs):
+		if self.cache is None or (self.lastp != p).any():
+			print '   recomputing cached model ... '
+			self.cache = self.othermodel.calc(p, left, right, *args, **kwargs)
+			self.lastp = left
 		return self.cache
 
 	def startup(self):
