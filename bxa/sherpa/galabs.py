fe8832c69fb00b67e55b2dbf4d0094d6b731f210
Johannes Buchner <johannes.buchner.acad@gmx.com>
2017-07-07 20:12:16 +0200

    unique galabs for fitting multiple spectra

--- bxa/sherpa/galabs.py
+++ bxa/sherpa/galabs.py
@@ -20,13 +20,14 @@ def auto_galactic_absorption(id=None):
 	filename = ui._session.get_data(id).name + '.nh'
 	print('loading nH from %s (expecting something like 1e21 in there)' % filename)
 	nH = float(open(filename).read().strip())
-	galabso = ui.xstbabs.galabso
+	galabso = ui.xstbabs('galabso%s' % id)
 	#galabsmodel = model * galabso
 	galabso.nH = nH / 1e22
 	print('setting galactic nH to %s [units of 1e22/cm²]' % (galabso.nH.val))
 	galabso.nH.freeze()
+	return galabso
 	cgalabso = CachedModel(galabso)
-	#print('returning cached galaxy absorption model')
+	print('returning cached galaxy absorption model')
 	return cgalabso
 	#ui._session.set_model(id, galabsmodel)
 
