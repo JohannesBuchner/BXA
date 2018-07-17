95e5864a83541554877e782abc96da66de836d15
Johannes Buchner <johannes.buchner.acad@gmx.com>
2018-07-17 16:58:07 -0700

    forgot to add docker test script

---
+++ examples/xspec/test-docker.sh
@@ -0,0 +1,27 @@
+
+# run like so:
+# docker run --rm -v $PWD/examples/xspec:/opt/script/ -v $PWD:/opt/BXA -ti giacomov/xspec-docker bash /opt/script/test-docker.sh
+
+export HEADAS=/heasoft/build/x86_64-unknown-linux-gnu-libc2.23-0/
+. $HEADAS/headas-init.sh
+
+cd /opt/
+apt-get update
+apt-get install -qq libblas{3,-dev} liblapack{3,-dev} libatlas{3-base,-dev} cmake build-essential gfortran python-numpy python-scipy python-matplotlib
+
+git clone https://github.com/JohannesBuchner/MultiNest
+mkdir -p MultiNest/build; pushd MultiNest/build; cmake .. && make && popd
+
+pip install pymultinest progressbar-latest
+#git clone https://github.com/JohannesBuchner/BXA
+pushd BXA; python setup.py install --user; popd
+
+ls /opt/MultiNest/lib/libmultinest.so
+export LD_LIBRARY_PATH=/opt/MultiNest/lib/:$LD_LIBRARY_PATH
+export PYTHONPATH=/usr/local/lib/python2.7/dist-packages/:$PYTHONPATH
+python -c 'import pymultinest';
+python -c 'import xspec';
+python -c 'import bxa.xspec as bxa';
+pushd /opt/script/ && bash -v runall.sh && popd;
+
+
