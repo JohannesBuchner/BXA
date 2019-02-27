342c80825ab24749c448f5b16060c8279cbc340a
Johannes Buchner <buchner.johannes@gmx.at>
2019-02-27 14:08:51 -0500

    added generic AGN script

--- examples/sherpa/xz_auto.py
+++ examples/sherpa/xz_auto.py
@@ -1,192 +1,233 @@
-from __future__ import print_function
+"""
+
+Fit a X-ray point source (e.g. AGN), potentially obscured with BXA
+
+
+How to run this script:
+
+1) enter the docker container:
+
+$ xhost +local:`docker inspect --format='{{ .Config.Hostname }}' johannesbuchner/bxa_absorbed`
+$ docker run -v  /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v $PWD:/opt/example/ -ti johannesbuchner/bxa_absorbed bash
+
+2) load ciao and run the script:
+
+root@7082a4c30d82:/opt/example# . /opt/ciao-4.8/bin/ciao.sh
+root@7082a4c30d82:/opt/example# FILENAME=combined_src.pi ELO=0.5 EHI=8 sherpa fitagn.py
+
+"""
+
+import bxa.sherpa as bxa
+from bxa.sherpa.background.pca import auto_background
+from sherpa.models.parameter import Parameter
+
+import logging
+logging.basicConfig(filename='bxa.log',level=logging.DEBUG)
+logFormatter = logging.Formatter("[%(name)s %(levelname)s]: %(message)s")
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+consoleHandler.setLevel(logging.INFO)
+logging.getLogger().addHandler(consoleHandler)
+
+ids = []
+
+id = 1
+filename = os.environ['FILENAME']
+#filename = 'combined_src.pi'
+prefix = filename + '_out_'
+load_pha(id, filename)
+ignore_id(id,None,None)
+notice_id(id,float(os.environ.get('ELO', 0.5)),float(os.environ.get('EHI',8)))
+ids.append(id)
+
+set_analysis(id, 'ener', 'counts')
+set_xlog()
+set_ylog()
+set_stat('cstat')
+set_xsabund('wilm')
+set_xsxsect('vern')
+
+
+id = ids[0]
+galabso = bxa.auto_galactic_absorption(id)
+galabso.nH.freeze()
+
+# Models available at https://doi.org/10.5281/zenodo.602282
+load_table_model("torus", '/home/user/Downloads/specmodels/uxclumpy-cutoff.fits')
+load_table_model("scat", '/home/user/Downloads/specmodels/uxclumpy-cutoff-omni.fits')
+# the limits correspond to fluxes between Sco X-1 and CDFS7Ms faintest fluxes
+srclevel = Parameter('src', 'level', 0, -8, 3, -20, 20) 
+print('combining components')
+model = torus + scat
+print('linking parameters')
+torus.norm = 10**srclevel
+srcnh = Parameter('src', 'nH', 22, 20, 26, 20, 26)
+torus.nh = 10**(srcnh - 22)
+scat.nh = torus.nh
+scat.nh = torus.nh
+print('setting redshift')
+redshift = Parameter('src', 'z', 1, 0, 5, 0, 10) 
+torus.redshift = redshift
+scat.redshift = redshift
+scat.phoindex = torus.phoindex
+
+scat.ecut = torus.ecut
+scat.theta_inc = torus.theta_inc
+scat.torsigma = torus.torsigma
+scat.ctkcover = torus.ctkcover
+softscatnorm = Parameter('src', 'softscatnorm', -2, -7, -1, -7, -1)
+scat.norm = 10**(srclevel + softscatnorm)
+
+
+
+
+print('creating priors')
+priors = []
+parameters = [srclevel, torus.phoindex, srcnh, softscatnorm]
+priors += [bxa.create_uniform_prior_for(srclevel)]
+priors += [bxa.create_gaussian_prior_for(torus.phoindex, 1.95, 0.15)]
+priors += [bxa.create_uniform_prior_for(srcnh)]
+priors += [bxa.create_uniform_prior_for(softscatnorm)]
+
+# 
+# apec with L(2-10keV) = 1e42 erg/s
+# z      norm 10keV    norm 2keV
+# 0.1    40e-6         150e-6
+# 0.5    2e-6          6e-6
+# 1      0.7e-6        2e-6
+# 3      0.15e-6       0.5e-6
+# ================================
+# z -->  0.5e-6/ z**2  2e-6/z**2
+# 
+if os.environ.get('WITHAPEC', '1') == '1':
+	model = model + xsapec.apec
+	apec.redshift = redshift
+	apec.kT.max = 8
+	apec.kT.min = 0.2
+	# normalised so that its luminosity does not go above 1e42 erg/s
+	apecnorm = Parameter('src', 'apecnorm', -2, -10, 0, -10, 0)
+	apec.norm = 10**apecnorm * 0.5e-6 / apec.redshift**2
+	prefix += 'withapec_'
+	parameters += [apecnorm, apec.kT]
+	priors += [bxa.create_uniform_prior_for(apecnorm)]
+	priors += [bxa.create_jeffreys_prior_for(apec.kT)]
+
+if os.path.exists(filename + '.z'):
+	redshift, zparameters, zpriorfs = bxa.prior_from_file(filename + '.z', redshift)
+	priors += zpriorfs
+	parameters += zparameters
+else:
+	prefix += 'zfree_'
+	parameters += [redshift]
+	priors += [bxa.create_uniform_prior_for(redshift)]
+
+
+assert len(priors) == len(parameters), 'priors: %d parameters: %d' % (len(priors), len(parameters))
+
+################
+# set model
+#    find background automatically using PCA method
+
+print('setting source and background model ...')
+id = 1
+set_model(id, model * galabso)
+convmodel = get_model(id)
+bkg_model = auto_background(id)
+set_full_model(id, get_response(id)(model) + bkg_model * get_bkg_scale(id))
+#plot_bkg_fit(id)
+
+## we allow the background normalisation to be a free fitting parameter
+p = bkg_model.pars[0]
+p.max = p.val + 2
+p.min = p.val - 2
+parameters.append(p)
+priors += [bxa.create_uniform_prior_for(p)]
+
+#################
+# BXA run
+priorfunction = bxa.create_prior_function(priors = priors)
+print('running BXA ...')
+
+bxa.nested_run(id = ids[0], otherids = tuple(ids[1:]),
+	prior = priorfunction, parameters = parameters, 
+	resume = True, verbose = True,
+	outputfiles_basename = prefix, n_live_points = os.environ.get('NLIVEPOINTS', 400),
+	importance_nested_sampling = False)
+
 try:
-	import bxa.sherpa as bxa
-	bxa.default_logging()
-	print('loading background fitting module...')
-	from bxa.sherpa.background.pca import auto_background
-	from bxa.sherpa.rebinnedmodel import RebinnedModel
-	from sherpa.models.parameter import Parameter
-
-	# load data: 
-	
-	# this is for SWIFT XRT, GRB data
-	datafile = os.environ.get('INPUTFILE', 'interval0pc.pi')
-	if datafile == 'interval0pc.pi':
-		# for swift we should load pc and wt spectra
-		id = 2
-		load_pha(id, 'interval0pc.pi')
-		if get_rmf(id).energ_lo[0] == 0: get_rmf(id).energ_lo[0] = 0.001
-		if get_arf(id).energ_lo[0] == 0: get_arf(id).energ_lo[0] = 0.001
-
-		id2 = None
-		otherids = ()
-		if os.path.exists('interval0wt.pi') and False:
-			id2 = 3
-			load_pha(id2, 'interval0wt.pi')
-			if get_rmf(id2).energ_lo[0] == 0: get_rmf(id2).energ_lo[0] = 0.001
-			if get_arf(id2).energ_lo[0] == 0: get_arf(id2).energ_lo[0] = 0.001
-			otherids = (id2,)
-			if not (get_bkg(id2).counts.sum() > 100):
-				delete_data(id2)
-				id2 = None
-				otherids = ()
-		ignore(None, 0.7)
-		ignore(7, None)
-	else:
-		# generic case
-		id = 2
-		load_pha(id, datafile)
-		if get_rmf(id).energ_lo[0] == 0: get_rmf(id).energ_lo[0] = 0.001
-		if get_arf(id).energ_lo[0] == 0: get_arf(id).energ_lo[0] = 0.001
-
-		id2 = None
-		otherids = ()
-		ignore(None, 0.5)
-		ignore(7, None)
-	
-	# set reasonable defaults
-	
-	set_stat('cstat')
-	set_xsabund('wilm')
-	set_xsxsect('vern')
-
-	# next we set up a source model.
-	#    with automatic milky way absorption
-	galabso = bxa.auto_galactic_absorption(id)
-	
-	################
-	# simplistic model
-	object_type = os.environ.get('object_type', 'AGN')
-	
-	if object_type == 'simplistic':
-		model = xszpowerlw.src * xszwabs.abso * galabso
-		model2 = model
-		srclevel = Parameter('src', 'level', numpy.log10(src.norm.val), -8, 2, -8, 2)
-		srcnh = Parameter('src', 'nh', numpy.log10(abso.nH.val)+22, 19, 24, 19, 24)
-		galnh = Parameter('gal', 'nh', numpy.log10(galabso.nH.val)+22, 19, 24, 19, 24)
-		src.PhoIndex.min = 1
-		src.PhoIndex.max = 3
-		src.PhoIndex.val = 2
-		src.norm.min = 1e-10
-		src.norm.max = 100
-		src.norm.val = 1e-5
-		src.redshift.min = 0
-		src.redshift.max = 10
-		abso.redshift = src.redshift
-		abso.nH.min = 1e19 / 1e22
-		abso.nH.max = 1e24 / 1e22
-		src.norm = 10**srclevel
-		abso.nH = 10**(srcnh - 22)
-
-		priors = []
-		parameters = [srclevel, src.PhoIndex, srcnh, src.redshift]
-		priors += [bxa.create_uniform_prior_for(srclevel)]
-		priors += [bxa.create_gaussian_prior_for(src.PhoIndex, 1.95, 0.15)]
-		priors += [bxa.create_uniform_prior_for(srcnh)]
-		priors += [bxa.create_uniform_prior_for(src.redshift)]
-	
-	################
-	# GRB & AGN model
-	if object_type in ['GRB', 'AGN']:
-		# use Murray Brightman's sphere model for GRB
-		load_table_model("sphere", os.environ.get('MODELDIR', '/opt/models') + '/sphere0708.fits')
-		load_table_model("sphere2", os.environ.get('MODELDIR', '/opt/models') + '/sphere0708.fits')
-		srclevel = Parameter('src', 'level', 0, -8, 3, -8, 3)
-		srclevel2 = Parameter('src2', 'level', 0, -8, 3, -8, 3)
-		print('combining components')
-		model = sphere * galabso
-		model2 = sphere2 * galabso
-		print('linking parameters')
-		sphere.norm = 10**srclevel
-		sphere2.norm = 10**srclevel2
-		srcnh = Parameter('src', 'nH', 22, 20, 26, 20, 26)
-		sphere.nH = 10**(srcnh - 22)
-		sphere2.nH = 10**(srcnh - 22)
-		print('setting redshift limits')
-		redshift = Parameter('src', 'z', 0, 0, 10, 0, 10)
-		sphere.redshift.max = 10
-		sphere2.redshift.max = 10
-		sphere.redshift = redshift
-		sphere2.redshift = redshift
-		
-		print('creating priors')
-		priors = []
-		parameters = [srclevel, sphere.phoindex, srcnh, redshift]
-		priors += [bxa.create_uniform_prior_for(srclevel)]
-		priors += [bxa.create_gaussian_prior_for(sphere.phoindex, 1.95, 0.15)]
-		priors += [bxa.create_uniform_prior_for(srcnh)]
-		priors += [bxa.create_uniform_prior_for(redshift)]
-		
-		if id2:
-			print('creating priors for second data model')
-			parameters += [srclevel2, sphere2.phoindex]
-			priors += [bxa.create_uniform_prior_for(srclevel2)]
-			priors += [bxa.create_gaussian_prior_for(sphere2.phoindex, 1.95, 0.15)]
-		
-		if object_type == 'AGN':
-			# add soft scattering pl for AGN
-			model = model + xszpowerlw.softscat
-			model2 = model2 + xszpowerlw.softscat2
-			softscatnorm = Parameter('src', 'softscatnorm', -2, -7, -1, -10, 0)
-			softscat.norm = sphere.norm * 10**softscatnorm
-			softscat.PhoIndex = sphere.phoindex
-			softscat.redshift = redshift
-			softscat2.PhoIndex = sphere2.phoindex
-			softscat2.norm = sphere2.norm * 10**softscatnorm
-			softscat2.redshift = redshift
-			
-			parameters.append(softscatnorm)
-			priors += [bxa.create_uniform_prior_for(softscatnorm)]
-	
-	assert len(priors) == len(parameters), 'priors: %d parameters: %d' % (len(priors), len(parameters))
-	
-	################
-	# set model
-	
-	#    find background automatically using PCA method
-	set_model(id, model)
-	convmodel = get_model(id)
-	bkg_model = auto_background(id)
-	set_full_model(id, convmodel + bkg_model*get_bkg_scale(id))
-
-	parameters.append(bkg_model.pars[0])
-	priors += [bxa.create_uniform_prior_for(bkg_model.pars[0])]
-	
-	if id2:
-		set_model(id2, model2)
-		convmodel2 = get_model(id2)
-		bkg_model2 = auto_background(id2)
-		set_full_model(id2, convmodel2 + bkg_model2*get_bkg_scale(id2))
-	
-		parameters.append(bkg_model2.pars[0])
-		priors += [bxa.create_uniform_prior_for(bkg_model2.pars[0])]
-	
-	show_model(id)
-	#################
-	# BXA run
-	priorfunction = bxa.create_prior_function(priors = priors)
-	print('running BXA ...')
-	prefix = os.environ.get('OUTPREFIX', '') + 'xz_%s_' % object_type
-	bxa.nested_run(id, otherids=otherids, prior=priorfunction, parameters = parameters, 
-		resume = True, verbose=True, 
-		outputfiles_basename = prefix,
-		importance_nested_sampling = False)
-
-	m = get_bkg_fit_plot(id)
-	numpy.savetxt(prefix + 'bkg.txt', numpy.transpose([m.dataplot.x, m.dataplot.y, m.modelplot.x, m.modelplot.y]))
-
-	m = get_fit_plot(id)
-	numpy.savetxt(prefix + 'xz_src.txt', numpy.transpose([m.dataplot.x, m.dataplot.y, m.modelplot.x, m.modelplot.y]))
-
-	if id2:
-		m = get_fit_plot(id2)
-		numpy.savetxt(prefix + 'xz_src2.txt', numpy.transpose([m.dataplot.x, m.dataplot.y, m.modelplot.x, m.modelplot.y]))
-
-	exit()
-
-
+	from mpi4py import MPI
+	if MPI.COMM_WORLD.Get_rank() > 0:
+		sys.exit(0)
 except Exception as e:
-	print('ERROR:', e)
-	exit()
+	pass
+
+outputfiles_basename = prefix
+print('getting best-fit ...')
+bxa.set_best_fit(parameters=parameters, outputfiles_basename=outputfiles_basename)
+
+
+for id in ids:
+	print('plotting spectrum ...')
+	set_analysis(id,'ener','counts')
+	m = get_bkg_fit_plot(id)
+	numpy.savetxt(prefix + 'bkg_'+str(id)+'.txt', numpy.transpose([m.dataplot.x, m.dataplot.y, m.modelplot.x, m.modelplot.y]))
+	m = get_fit_plot(id)
+	numpy.savetxt(prefix + 'src_'+str(id)+'.txt', numpy.transpose([m.dataplot.x, m.dataplot.y, m.modelplot.x, m.modelplot.y]))
+
+# compute 2-10keV intrinsic luminosities?
+thawedpars = parameters
+print("calculating intrinsic fluxes and distribution of model spectra")
+# calculate restframe intrinsic flux
+id = ids[0]
+set_model(id, torus)
+
+import pymultinest
+a = pymultinest.analyse.Analyzer(n_params = len(thawedpars),
+	outputfiles_basename = prefix)
+rows = a.get_equal_weighted_posterior()
+r = []
+for i, row in enumerate(rows):
+	sys.stdout.write("%d/%d (%.2f%%)\r" % (i, len(rows), (i + 1)*100./ len(rows)))
+	sys.stdout.flush()
+	z = redshift.val if hasattr(redshift, 'val') else redshift
+	for p, v in zip(thawedpars, row):
+		if p.name == 'redshift' or p.name == 'z':
+			z = v
+		p.val = v
+
+	srcnh.val = 20
+	r.append([z, 
+		calc_energy_flux(id=id, lo=2/(1+z), hi=10/(1+z)),
+		calc_energy_flux(id=id, lo=0.5/(1+z), hi=8/(1+z))
+	])
+
+print("saving distribution plot data")
+r = numpy.asarray(r)
+assert len(rows) == len(r)
+numpy.savetxt(prefix + "intrinsic_photonflux.dist", r)
+
+rows = a.get_data()[:,2:]
+r = []
+for i, row in enumerate(rows):
+	sys.stdout.write("%d/%d (%.2f%%)\r" % (i, len(rows), (i + 1)*100./ len(rows)))
+	sys.stdout.flush()
+	z = redshift.val if hasattr(redshift, 'val') else redshift
+	for p, v in zip(thawedpars, row):
+		if p.name == 'redshift' or p.name == 'z':
+			z = v
+		p.val = v
+
+	srcnh.val = 20
+	r.append([z, 
+		calc_energy_flux(id=id, lo=2/(1+z), hi=10/(1+z)),
+		calc_energy_flux(id=id, lo=0.5/(1+z), hi=8/(1+z))
+	])
+
+print("saving distribution plot data")
+r = numpy.asarray(r)
+assert len(rows) == len(r)
+numpy.savetxt(prefix + "intrinsic_photonflux_weighted.dist", r)
+
+exit()
 
 
