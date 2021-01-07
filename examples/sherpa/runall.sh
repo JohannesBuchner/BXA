a0c53623d3401e19427b7dfb139d34aa19357ab1
Johannes Buchner <johannes.buchner.acad@gmx.com>
2021-01-07 18:29:25 +0100

    [ci] forgot nh file

--- examples/sherpa/runall.sh
+++ examples/sherpa/runall.sh
@@ -1,5 +1,7 @@
 export PYTHONPATH=../../:$PYTHONPATH:../../npyinterp/ LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../npyinterp/ 
 
+echo 1.680000e+20 > swift/interval0pc.pi.nh
+
 coverage run -p example_simplest.py || exit 1
 coverage run -p example_pcabackground.py || exit 2
 coverage run -p example_automatic_background_model.py || exit 3
