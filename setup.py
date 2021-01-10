3b7205c3e8056bbb78bae4872af9d62f4827ec4f
Johannes Buchner <johannes.buchner.acad@gmx.com>
2021-01-10 23:55:44 +0100

    Bump version: 4.0.0 → 4.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 	long_description = ''
 
 setup(name='bxa',
-	version='4.0.0',
+	version='4.0.1',
 	author='Johannes Buchner',
 	url='https://github.com/JohannesBuchner/BXA/',
 	author_email='johannes.buchner.acad@gmx.com',
